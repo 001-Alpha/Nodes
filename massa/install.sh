1386f8721aee49dd06201a42c23c6765d57e4968
FairyTale#5571 <onopa96@gmail.com>
2022-04-27 19:14:39 +0300

    massa

--- massa/install.sh
+++ massa/install.sh
@@ -18,5 +18,29 @@ if [ ! -d $HOME/massa/ ]; then
 
 cd massa/massa-node/
 screen -S massa_node
+
+cargo build --release
+
+
 RUST_BACKTRACE=full cargo run --release |& tee logs.txt
 
+sudo systemctl restart systemd-journald
+sudo systemctl enable massa
+sudo systemctl daemon-reload
+sudo systemctl restart massa
+
+cd $HOME/massa/massa-client/
+cargo run -- --wallet wallet.dat wallet_generate_private_key
+
+echo "alias client='cd $HOME/massa/massa-client/ && cargo run --release && cd'" >> ~/.profile
+echo "alias clientw='cd $HOME/massa/massa-client/; cargo run -- --wallet wallet.dat; cd'" >> ~/.profile
+
+cd $HOME
+mkdir -p $HOME/backups
+cp $HOME/massa/massa-node/config/node_privkey.key $HOME/backups/
+cp $HOME/massa/massa-client/wallet.dat $HOME/backups/
+if [ ! -e $HOME/massa_bk.tar.gz ]; then
+	tar cvzf massa_bk.tar.gz backups
+fi
+
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/massa/bootstrap-fix.sh | bash
